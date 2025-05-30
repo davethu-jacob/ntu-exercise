22c39b1ea482ed56dad89a219bf781a126cd5ac0
Davethu <davethu@gmail.com>
2025-05-30 14:23:16 +0800

    updated MyCircle and TestMyCircle classes as per ex 2.8

--- src/main/java/com/composition/MyTriangle.java
+++ src/main/java/com/composition/MyTriangle.java
@@ -1,9 +1,9 @@
 package com.composition;
 
 public class MyTriangle {
-    private MyPoint v1;
-    private MyPoint v2;
-    private MyPoint v3;
+    private final MyPoint v1;
+    private final MyPoint v2;
+    private final MyPoint v3;
 
     public MyTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
         v1 = new MyPoint(x1, y1);
@@ -21,4 +21,31 @@ public class MyTriangle {
     public String toString() {
         return String.format("MyTriangle[v1=%s,v2=%s,v3=%s]", v1, v2, v3);
     }
+
+    public double getPerimeter() {
+        double l1 = v1.distance(v2);
+        double l2 = v2.distance(v3);
+        double l3 = v3.distance(v1);
+        System.out.println("Length l1: " + l1);
+        System.out.println("Length l2: " + l2);
+        System.out.println("Length l3: " + l3);
+        return l1 + l2 + l3;
+    }
+
+    public String getType() {
+        Double l1 = v1.distance(v2);
+        Double l2 = v2.distance(v3);
+        Double l3 = v3.distance(v1);
+        String type = "Scalene";
+        System.out.println(l1.equals(l2));
+        System.out.println(l2.equals(l3));
+        System.out.println(l3.equals(l1));
+        if (l1.equals(l2) && l2.equals(l3) && l3.equals(l1)) {
+            type = "Equilateral";
+        } else if (l1.equals(l2) || l2.equals(l3) || l3.equals(l1)) {
+            type = "Isosceles";
+        }
+        return type;
+    }
 }
+
--- src/main/java/com/composition/TestMyTriangle.java
+++ src/main/java/com/composition/TestMyTriangle.java
@@ -10,5 +10,15 @@ public class TestMyTriangle {
         MyPoint p3 = new MyPoint(6, 6);
         MyTriangle t2 = new MyTriangle(p1, p2, p3);
         System.out.println(t2);
+
+        System.out.printf("Perimeter of triangle t2 is: %.2f%n", t2.getPerimeter());
+
+        System.out.println("Triangle type is: " + t2.getType());
+
+        MyPoint p4 = new MyPoint(0,0);
+        MyPoint p5 = new MyPoint(2,2);
+        MyPoint p6 = new MyPoint(3,-4);
+        MyTriangle t3 = new MyTriangle(p4, p5, p6);
+        System.out.println("Triangle type is: " + t3.getType());
     }
 }
