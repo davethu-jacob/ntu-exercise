6b0f43d8f139dd045382c2fe25495934041f7054
Davethu <davethu@gmail.com>
2025-06-06 17:39:24 +0800

    Completed exercise 3.1 and started on exercise 3.2

--- src/main/java/com/composition/MyTime.java
+++ src/main/java/com/composition/MyTime.java
@@ -1,5 +1,7 @@
 package com.composition;
 
+import com.oop.Time;
+
 public class MyTime {
     private int hour;
     private int minute;
@@ -15,9 +17,13 @@ public class MyTime {
     }
 
     public void setTime(int hour, int minute, int second) {
-        this.hour = hour;
-        this.minute = minute;
-        this.second = second;
+        if ((hour < 0 || hour > 23 || minute < 0 || minute > 59 || second < 0 || second > 59))
+            System.out.println("Invalid time");
+        else {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
     }
 
     public int getHour() {
@@ -25,7 +31,9 @@ public class MyTime {
     }
 
     public void setHour(int hour) {
-        this.hour = hour;
+        if (hour < 0 || hour > 23) System.out.println("Hour must be between 0 and 23 inclusive");
+        else
+            this.hour = hour;
     }
 
     public int getMinute() {
@@ -43,8 +51,7 @@ public class MyTime {
     }
 
     public void setSecond(int second) {
-        if (second < 0 || second > 59)
-            System.out.println("Second must be between 0 and 59 inclusive");
+        if (second < 0 || second > 59) System.out.println("Second must be between 0 and 59 inclusive");
         else
             this.second = second;
     }
@@ -53,4 +60,79 @@ public class MyTime {
     public String toString() {
         return String.format("%02d:%02d:%02d", hour, minute, second);
     }
+
+    public MyTime nextSecond() {
+        ++second;
+        if (second >= 60) {
+            second = 0;
+            ++minute;
+            if (minute >= 60) {
+                minute = 0;
+                ++hour;
+                if (hour >= 24) {
+                    hour = 0;
+                }
+            }
+        }
+        return this;
+    }
+
+    public MyTime nextMinute() {
+        ++minute;
+        if (minute >= 60) {
+            minute = 0;
+            ++hour;
+            if (hour >= 24) {
+                hour = 0;
+            }
+        }
+        return this;
+    }
+
+    public MyTime nextHour() {
+        ++hour;
+        if (hour >= 24) {
+            hour = 0;
+        }
+        return this;
+    }
+
+    public MyTime previousSecond() {
+        --second;
+        if (second < 0) {
+            second = 59;
+            --minute;
+            if (minute < 0) {
+                minute = 59;
+                --hour;
+                if (hour < 0) {
+                    hour = 23;
+                }
+            }
+        }
+        return this;
+    }
+
+    public MyTime previousMinute() {
+        --minute;
+        if (minute < 0) {
+            minute = 59;
+            --hour;
+            if (hour < 0) {
+                hour = 23;
+            }
+        }
+        return this;
+    }
+
+    public MyTime previousHour() {
+        --hour;
+        if (hour < 0) {
+            hour = 23;
+        }
+        return this;
+    }
+
+
+
 }
--- src/main/java/com/composition/TestMyTime.java
+++ src/main/java/com/composition/TestMyTime.java
@@ -12,9 +12,28 @@ public class TestMyTime {
         t1.setMinute(60);
         t1.setMinute(45);
         System.out.println(t1);
+        t1.setHour(-1);
+        t1.setHour(24);
+        t1.setHour(18);
+        System.out.println(t1);
 
         MyTime t2 = new MyTime(22, 50, 59);
         System.out.println(t2);
+        t2.setTime(23, 59, 59);
+        System.out.println(t2);
+        t2.setTime(24, 55, 55);
+        System.out.println(t2);
+
+        System.out.println(t2.nextSecond().nextSecond());
+        System.out.println(t2.previousSecond().previousSecond().previousSecond());
+
+        System.out.println(t2.previousMinute().previousMinute());
+        System.out.println(t2.previousMinute().previousMinute().previousMinute().previousMinute());
+        t2.setTime(0, 0, 0);
+        System.out.println(t2.previousMinute().previousMinute());
+        System.out.println(t2.nextMinute().nextMinute().previousMinute());
+
+        System.out.println(t2.nextHour().nextHour().previousHour().previousHour());
 
     }
 }
