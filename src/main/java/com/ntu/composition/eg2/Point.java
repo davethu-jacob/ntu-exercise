dc61f1385a46903bda7ea1b0c97a32d31a708566
davethudjacob <davethu@gmail.com>
2025-06-25 22:45:07 +0800

    Completed classes Point and TestPoint

--- src/main/java/com/ntu/composition/eg2/Point.java
+++ src/main/java/com/ntu/composition/eg2/Point.java
@@ -32,4 +32,35 @@ public class Point {
     public String toString() {
         return String.format("(%d,%d)", x, y);
     }
+
+    public int[] getXY() {
+        int[] result = new int[2];
+        result[0] = x;
+        result[1] = y;
+        return result;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(int x, int y) {
+        int xDiff = this.x - x;
+        int yDiff = this.y - y;
+        return Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+    }
+
+    public double distance(Point another) {
+        int xDiff = another.x - this.x;
+        int yDiff = another.y - this.y;
+        return Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+    }
+
+    public double distance() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+
+
+
 }
--- src/main/java/com/ntu/composition/eg2/TestPoint.java
+++ src/main/java/com/ntu/composition/eg2/TestPoint.java
@@ -1,11 +1,23 @@
 package com.ntu.composition.eg2;
 
+import java.util.Arrays;
+
 public class TestPoint {
     public static void main(String[] args) {
         Point p1 = new Point();
         System.out.println(p1);
+        p1.setX(1);
+        p1.setY(1);
+        System.out.println(p1);
+        p1.setXY(2, 2);
+        System.out.println(p1);
+        System.out.println(p1.distance(8, 10));
 
-        Point p2 = new Point(2, 5);
+        Point p2 = new Point(8, 10);
         System.out.println(p2);
+        System.out.println(Arrays.toString(p2.getXY()));
+        System.out.println(p1.distance(p2));
+        Point p3 = new Point(12, 0);
+        System.out.println(p3.distance());
     }
 }
