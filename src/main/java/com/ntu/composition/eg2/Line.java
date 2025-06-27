fd0113aabfc273f3d150a665271524d39fc2d8e0
davethudjacob <davethu@gmail.com>
2025-06-27 15:40:34 +0800

    Completed eg 1.2

--- src/main/java/com/ntu/composition/eg2/Line.java
+++ src/main/java/com/ntu/composition/eg2/Line.java
@@ -30,8 +30,68 @@ public class Line {
         this.end = end;
     }
 
+    public int getBeginX() {
+        return begin.getX();
+    }
+
+    public void setBeginX(int x) {
+        begin.setX(x);
+    }
+
+    public int getBeginY() {
+        return begin.getY();
+    }
+
+    public void setBeginY(int y) {
+        begin.setY(y);
+    }
+
+    public int[] getBeginXY() {
+        int[] result = new int[2];
+        result[0] = begin.getX();
+        result[1] = begin.getY();
+        return result;
+    }
+
+    public void setBeginXY(int x, int y) {
+        begin.setXY(x, y);
+    }
+
+    public int getEndX() {
+        return end.getX();
+    }
+
+    public void setEndX(int x) {
+        end.setX(x);
+    }
+
+    public int getEndY() {
+        return end.getY();
+    }
+
+    public void setEndY(int y) {
+        end.setY(y);
+    }
+
+    public int[] getEndXY() {
+        int[] result = new int[2];
+        result[0] = end.getX();
+        result[1] = end.getY();
+        return result;
+    }
+
+    public void setEndXY(int x, int y) {
+        end.setXY(x, y);
+    }
+
     @Override
     public String toString() {
         return String.format("Line[begin=%s,end=%s]", begin.toString(), end.toString());
+    }
+
+    public double getLength() {
+        int xDiff = end.getX() - begin.getX();
+        int yDiff = end.getY() - begin.getY();
+        return Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
     }
 }
