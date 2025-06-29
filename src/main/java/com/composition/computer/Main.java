af1b96c4fc2a5400db6b03e783d40df24e549c9f
davethudjacob <davethu@gmail.com>
2025-06-29 21:50:21 +0800

    Completed Computer

--- src/main/java/com/composition/computer/Main.java
+++ src/main/java/com/composition/computer/Main.java
@@ -4,6 +4,7 @@ package com.composition.computer;
 /* Composition in Java is a fundamental object-oriented programming (OOP) principal used to establish a "has-a" relationship between classes.
  * It involves one class containing an object of another class as an instant variable, rather than inheriting from it.
  * This approach promotes loose coupling and enhances flexibility, maintainability, and reusability in software design.
+ *
  * Key characteristics of Composition
  * ==================================
  * 1. "Has-a" Relationship - This signifies that an object of one class "has" an object of another class. for example, a Computer object "has an" Processor object.
@@ -11,6 +12,7 @@ package com.composition.computer;
  * and may not be able to exist independently if the containing object is destroyed.
  * 3. Achieved via Instance Variables - Composition is implemented by declaring an instance of one class within another class.
  * 4. Delegation of Responsibility - The containing class can delegate specific functionalities to the contained object, leveraging its methods and data.
+ *
  */
 
 public class Main {
