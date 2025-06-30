4365d039a9f2a668d68f27441821fa56f51901d9
davethudjacob <davethu@gmail.com>
2025-06-30 23:22:36 +0800

    Partially completed ex 2.5

---
+++ src/main/java/com/ntu/composition/ex25/TestAccount.java
@@ -0,0 +1,9 @@
+package com.ntu.composition.ex25;
+
+public class TestAccount {
+    public static void main(String[] args) {
+        Customer customer = new Customer(1, "NCR Malaysia", 'M');
+        System.out.println(customer);
+
+    }
+}
