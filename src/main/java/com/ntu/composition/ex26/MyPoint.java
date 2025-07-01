5f19fe054663c0c9cca83548c33c566b987f8a40
davethudjacob <davethu@gmail.com>
2025-07-01 22:47:32 +0800

    Partially completed ex 2.6

---
+++ src/main/java/com/ntu/composition/ex26/MyPoint.java
@@ -0,0 +1,47 @@
+package com.ntu.composition.ex26;
+
+public class MyPoint {
+    private int x = 0;
+    private int y = 0;
+
+    public MyPoint() {
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int[] getXY() {
+        int[] result = new int[2];
+        result[0] = x;
+        result[1] = y;
+        return result;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d,%d)", x, y);
+    }
+}
