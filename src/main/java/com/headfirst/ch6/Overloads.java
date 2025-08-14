8b4e96bc168d027369f8a73809ebbb6db3626afd
Davethu <davethu@gmail.com>
2025-08-14 11:45:34 +0800

    Completed MonsterTestDrive in chapter 7

---
+++ src/main/java/com/headfirst/ch6/Overloads.java
@@ -0,0 +1,30 @@
+package com.headfirst.ch6;
+
+class Overloads {
+    String uniqueID;
+
+    int addNumber(int a, int b) {
+        return a + b;
+    }
+
+    double addNumber(double a, double b) {
+        return a + b;
+    }
+
+    void setUniqueID(String uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+
+    void setUniqueID(int ssNumber) {
+        String numString = "" + ssNumber;
+        setUniqueID(numString);
+    }
+
+    public static void main(String[] args) {
+        Overloads ol = new Overloads();
+        System.out.println(ol.addNumber(1, 2));
+        System.out.println(ol.addNumber(1, 2));
+        System.out.println(ol.addNumber(1.0, 2.0));
+        System.out.println(ol.addNumber(1.05, 2.18));
+    }
+}
